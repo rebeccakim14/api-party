f5c17ea452005c10ed3138a06875ddbd9ab08864
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-07-10 15:43:52 -0400

    add a switch with a default case

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 
 import './App.css'
 
@@ -11,17 +11,19 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </header>
-
         <ul className="navLinks">
           <li>
             <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
-
-        <Route path="/github" render={() => <h1>GitHub!</h1>} />
+        <Switch>
+          <Route path="/github" render={() => <h1>GitHub!</h1>} />
+          <Route render={() => (
+            <p>To get started, click one of the links above.</p>
+          )}/>
+        </Switch>
       </div>
     )
   }
 }
-
 export default App
